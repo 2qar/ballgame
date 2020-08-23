6470e9f61595eb4f1fa8811dbf423d5faf487e9e
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-08-23 11:55:22 -0700

    make the ball an Area2D instead of a RigidBody2D
    this should hopefully make syncing position a little less painful
    because the physics simulation stuff isn't an issue with Area2D

--- ball.gd
+++ ball.gd
@@ -1,21 +1,21 @@
-# TODO: switch to KinematicBody2D, idiot
-extends RigidBody2D
+extends Area2D
 
 signal goal_hit(pos)
 
 export var hit_velocity : int
+var velocity = Vector2()
 export(int, LAYERS_2D_PHYSICS) var still_mask
 export(int, LAYERS_2D_PHYSICS) var moving_mask
 
 onready var initial_pos = position
-var reset = false
 
 func _ready():
 	collision_layer = still_mask
 	collision_mask = still_mask
+	connect("body_entered", self, "_on_ball_body_entered")
 
 remotesync func hit(dir: Vector2):
-	linear_velocity = dir * hit_velocity
+	velocity = dir * hit_velocity
 	$trail.process_material.direction = Vector3(dir.x, dir.y, 0)
 	collision_layer = moving_mask
 	collision_mask = moving_mask
@@ -23,24 +23,19 @@ remotesync func hit(dir: Vector2):
 func _draw():
 	draw_circle(Vector2(), $collider.shape.radius, Color.beige)
 
-func _integrate_forces(state):
-	if reset:
-		reset_position(state)
-	
-	$trail.process_material.initial_velocity = state.linear_velocity.length()
-	if state.linear_velocity.sign().x == 1 or state.linear_velocity.sign().y == 1:
-		$trail.process_material.initial_velocity *= -1
+func _physics_process(delta):
+	position += velocity
 
-func reset_position(state: Physics2DDirectBodyState):
-	state.linear_velocity = Vector2()
-	state.transform = Transform2D(0, initial_pos)
-	collision_layer = still_mask
-	collision_mask = still_mask
-	$trail.restart()
-	reset = false
+	$trail.process_material.initial_velocity = velocity.length()
+	if velocity.sign().x == 1 or velocity.sign().y == 1:
+		$trail.process_material.initial_velocity *= -1
 
 func _on_ball_body_entered(body):
 	if body.name == "goal":
 		emit_signal("goal_hit", position)
 	else:
-		reset = true
+		velocity = Vector2()
+		position = initial_pos
+		collision_layer = still_mask
+		collision_mask = still_mask
+		$trail.restart()
